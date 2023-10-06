6ac380780c779e3b3dd0b9ff6a362dee4b95937c
Jason Shui <jiashui@hotmail.com>
2023-10-06 12:29:54 +0800

    chore: add clangd to script, add .vscode folder to unify configuration
    Signed-off-by: Jason Shui <jiashui@hotmail.com>

--- scripts/install_env.bash
+++ scripts/install_env.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-sudo apt install -y libc++abi-17-dev libc++-17-dev lldb-17
+sudo apt install -y libc++abi-17-dev libc++-17-dev lldb-17 clangd-17
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-17 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-17 100
 sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-17 100
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-17 100
 clang -v
 clang++ -v
